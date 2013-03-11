29f91bbddd65604d8c7d45c13a5cc766bf2a69b2
gunitzz <edison890823@msn.com>
2013-03-11 14:15:13 -0400

    update documentindexed

--- HW2/src/edu/nyu/cs/cs2580/DocumentIndexed.java
+++ HW2/src/edu/nyu/cs/cs2580/DocumentIndexed.java
@@ -1,21 +1,36 @@
 package edu.nyu.cs.cs2580;
 
-import java.util.Vector;
-import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  * @CS2580: implement this class for HW2 to incorporate any additional
- * information needed for your favorite ranker.
+ *          information needed for your favorite ranker.
  */
 public class DocumentIndexed extends Document {
-  private static final long serialVersionUID = 9184892508124423115L;
-  private Indexer _indexer = null;
-  private HashMap<String, Integer> docTermFrequecy = null;
- 
-  public DocumentIndexed(int docid, Indexer indexer) {
-    super(docid);
-      _indexer = indexer;
-      
-  }
+	private static final long serialVersionUID = 9184892508124423115L;
+	private Indexer _indexer = null;
+	private HashMap<String, Integer> docTermFrequency = null;
+
+	public DocumentIndexed(int docid, Indexer indexer) {
+		super(docid);
+		_indexer = indexer;
+		docTermFrequency = new HashMap<String, Integer>();
+	}
+
+	/**
+	 * @return the docTermFrequency
+	 */
+	public HashMap<String, Integer> getDocTermFrequency() {
+		return docTermFrequency;
+	}
+
+	/**
+	 * @param docTermFrequency the docTermFrequency to set
+	 */
+	public void setDocTermFrequency(HashMap<String, Integer> docTermFrequency) {
+		this.docTermFrequency = docTermFrequency;
+	}
+	
+	
 
 }
