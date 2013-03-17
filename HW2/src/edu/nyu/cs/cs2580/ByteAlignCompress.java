062279b17b58c4c3e4c0257ca35650970580cb67
Xiyuan <xiyuan.huhu@gmail.com>
2013-03-16 22:31:32 -0400

    download file

--- HW2/src/edu/nyu/cs/cs2580/ByteAlignCompress.java
+++ HW2/src/edu/nyu/cs/cs2580/ByteAlignCompress.java
@@ -1,8 +1,8 @@
 /*************************************************************************
-    > File Name: ByteAlignCompress.java
-    > Author: Xiao Cui
-    > Mail: xc432@nyu.edu 
-    > Created Time: Wed Mar  6 14:46:25 2013
+ > File Name: ByteAlignCompress.java
+ > Author: Xiao Cui
+ > Mail: xc432@nyu.edu 
+ > Created Time: Wed Mar  6 14:46:25 2013
  ************************************************************************/
 package edu.nyu.cs.cs2580;
 import java.util.ArrayList;
@@ -10,126 +10,138 @@ import java.util.List;
 import java.util.LinkedList;
 import java.io.Serializable;
 import java.lang.Math;
+import java.io.IOException;
 public class ByteAlignCompress implements Serializable{
-  private static final long serialVerisionUID = 1099111905740087931L;  
-  //private ArrayList<Byte> byteList;
-  //private List<Integer> intList;
-
-   public ByteAlignCompress(){
-//	byteList = new ArrayList<Byte>();
-//	intList = new ArrayList<Integer>();
-   }
-  
-  public ArrayList<Byte> compressSingle(int para){
-      ArrayList<Byte> byteList = new ArrayList<Byte>();   
-	  /*if(para >= 1<<21){
-          byteList.add((byte)(para>>21));
-	  }
-	  para&=(1<<21)-1;
-
-	  if(para>=1<<14){
-		  byteList.add((byte)(para>>14));
-	  }
-      para&=(1<<14)-1;
-
-	  if(para>=1<<7){
-		  byteList.add((byte)(para>>7));
-	  }
-	  para&=(1<<7)-1;
-
-	  para|=128;
-	  byte parab = (byte)(para);
-	  byteList.add(parab);
-	  return byteList;
-    */
-	  boolean first = true;
-	  if(para == 0){
-		  byteList.add((byte)(1<<7));
-	  }
-	  while(para > 0 ){
-		  byte parab = (byte)(para % 128);
-          para = para >> 7;
-		  if(first){
-			  parab |= 1 << 7;
-			  first = false;
-		  }
-		  byteList.add(parab);
-	  }
-	  return byteList;
-  
-  }
-
- /* public void compressList(List<Integer> postList){
-      for(Integer para: postList){
-      if(para >= 1<<21){
-          byteList.add((byte)(para>>21));
-	  }
-	  para&=(1<<21)-1;
-
-	  if(para>=1<<14){
-		  byteList.add((byte)(para>>14));
-	  }
-      para&=(1<<14)-1;
-
-	  if(para>=1<<7){
-		  byteList.add((byte)(para>>7));
-	  }
-	  para&=(1<<7)-1;
-
-	  para|=128;
-	  byteList.add(para.byteValue());
-  
+    private static final long serialVerisionUID = 1099111905740087931L;  
+    //private ArrayList<Byte> byteList;
+    //private List<Integer> intList;
+    
+    public ByteAlignCompress(){
+        //	byteList = new ArrayList<Byte>();
+        //	intList = new ArrayList<Integer>();
+    }
+    
+    public ArrayList<Byte> compressSingle(int para){
+        ArrayList<Byte> byteList = new ArrayList<Byte>();   
+        /*if(para >= 1<<21){
+         byteList.add((byte)(para>>21));
+         }
+         para&=(1<<21)-1;
+         
+         if(para>=1<<14){
+         byteList.add((byte)(para>>14));
+         }
+         para&=(1<<14)-1;
+         
+         if(para>=1<<7){
+         byteList.add((byte)(para>>7));
+         }
+         para&=(1<<7)-1;
+         
+         para|=128;
+         byte parab = (byte)(para);
+         byteList.add(parab);
+         return byteList;
+         */
+        boolean first = true;
+        if(para == 0){
+            byteList.add((byte)(1<<7));
+        }
+        while(para > 0 ){
+            byte parab = (byte)(para % 128);
+            para = para >> 7;
+            if(first){
+                parab |= 1 << 7;
+                first = false;
+            }
+            byteList.add(parab);
+        }
+        return byteList;
+        
+    }
+    
+    /* public void compressList(List<Integer> postList){
+     for(Integer para: postList){
+     if(para >= 1<<21){
+     byteList.add((byte)(para>>21));
      }
-  }*/
-  
-   public int decompress(ArrayList<Byte> byteList){
-       if(byteList == null)
-		   return -1;
-	   
-	   int temp = 0;
-	   /*for(int i = 0; i < byteList.size(); i++){
-           //int temp = 0;
-           while(byteList.get(i) > 0){
-               temp = temp << 7;
-               temp += byteList.get(i);
-               i++;
-           }
-           temp = temp << 7;
-           temp += byteList.get(i)&127;
-          // intList.add(temp);
-       }*/
+     para&=(1<<21)-1;
+     
+     if(para>=1<<14){
+     byteList.add((byte)(para>>14));
+     }
+     para&=(1<<14)-1;
+     
+     if(para>=1<<7){
+     byteList.add((byte)(para>>7));
+     }
+     para&=(1<<7)-1;
+     
+     para|=128;
+     byteList.add(para.byteValue());
+     
+     }
+     }*/
+    
+    public int decompress(ArrayList<Byte> byteList){
+        if(byteList == null)
+            return -1;
+        
+        int temp = 0;
+        /*for(int i = 0; i < byteList.size(); i++){
+         //int temp = 0;
+         while(byteList.get(i) > 0){
+         temp = temp << 7;
+         temp += byteList.get(i);
+         i++;
+         }
+         temp = temp << 7;
+         temp += byteList.get(i)&127;
+         // intList.add(temp);
+         }*/
 		temp += (int)(byteList.get(0)&((1<<7)-1));
 		for(int i = 1; i<byteList.size();i++){
 			temp += byteList.get(i)*((int)Math.pow(128,i));
 		}
-	   return temp;
-   }
-  
-   public int decompressID(ArrayList<Byte> byteList){
-	   if(byteList.size() == 0)
-		   System.out.println("No doc id inside");
-	   ArrayList<Byte> did = new ArrayList<Byte>();
-	   did.add(byteList.get(0));
-	   for(int i = 1; i < byteList.size(); i++){
-		   if((byteList.get(i)&(1<<7)) > 0)
-			   break;
-		   did.add(byteList.get(i));
-	   }
-	   return decompress(did);
-   }
-
- /*   public static void main(String args[]){
-        ByteAlignCompress b = new ByteAlignCompress();
-        List<Integer> postList = new LinkedList<Integer>();
-        postList.add(1);
-        postList.add(6);
-        ArrayList<Byte> by =  b.compressSingle(1);
-        for(Byte i:  by){
-            System.out.println(Integer.toHexString(i));
+        return temp;
+    }
+    
+    public int decompressID(ArrayList<Byte> byteList){
+        if(byteList.size() == 0)
+            System.out.println("No doc id inside");
+        ArrayList<Byte> did = new ArrayList<Byte>();
+        did.add(byteList.get(0));
+        for(int i = 1; i < byteList.size(); i++){
+            if((byteList.get(i)&(1<<7)) > 0)
+                break;
+            did.add(byteList.get(i));
         }
-        /*b.decompress(b.byteList);
-        for(Integer i: b.intList){
-            System.out.println(Integer.toString(i));
+        return decompress(did);
+    }
+    
+    public ArrayList<Integer> decompressTermIDs(ArrayList<ArrayList<Byte>> infoindex) throws IOException{
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+        for(ArrayList<Byte> list: infoindex){
+            int docid;
+            docid = decompressID(list);
+            ids.add(docid);
         }
-    }*/
+        return ids;
+        
+    }
+    
+    /*   public static void main(String args[]){
+     ByteAlignCompress b = new ByteAlignCompress();
+     List<Integer> postList = new LinkedList<Integer>();
+     postList.add(1);
+     postList.add(6);
+     ArrayList<Byte> by =  b.compressSingle(1);
+     for(Byte i:  by){
+     System.out.println(Integer.toHexString(i));
+     }
+     /*b.decompress(b.byteList);
+     for(Integer i: b.intList){
+     System.out.println(Integer.toString(i));
+     }
+     }*/
 }
--- HW2/src/edu/nyu/cs/cs2580/QueryPhrase.java
+++ HW2/src/edu/nyu/cs/cs2580/QueryPhrase.java
@@ -1,44 +1,119 @@
 package edu.nyu.cs.cs2580;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Vector;
 /**
  * @CS2580: implement this class for HW2 to handle phrase. If the raw query is
  * ["new york city"], the presence of the phrase "new york city" must be
  * recorded here and be used in indexing and ranking.
  */
 public class QueryPhrase extends Query {
-	// Store the start indices of a phrase in _tokens
-	public ArrayList<Integer> startIndex = new ArrayList<Integer>();
-	// Store the end indices of a phrase in _tokens
-	public ArrayList<Integer> endIndex = new ArrayList<Integer>();
-	
+    
+    /*    public Vector<String[]> phrases = new Vector<String[]>();
+     public Vector<String> nonPhrase = new Vector<String>();
+     
+     public Vector<String> getNonPhrase(){
+     return nonPhrase;
+     }
+     
+     public Vector<String[]> getPhrases(){
+     return phrases;
+     }
+     */
 	public QueryPhrase(String query) {
 		super(query);
 	}
-
+    
+	/*public int getPhraseCount(){
+     return phraseCount;
+     }*/
 	@Override
 	public void processQuery() {
 		if (_query == null)
 			return;
-
-		Scanner s = new Scanner(_query);
-		int i = 0;
-		while (s.hasNext()) {
-			String result = s.next();
-			if (result.startsWith("\"")) {
-				startIndex.add(i);
-				result = result.substring(1); // remove the quote
-			} 
-			if (result.endsWith("\"")) {
-				endIndex.add(i);
-				result = result.substring(0, result.length() - 1); // remove the quote
-			}
-			_tokens.add(result);
-			i++;
-		}
-		s.close();
+        
+	    Pattern p = Pattern.compile("\"(.*?)\"");
+		Matcher match = p.matcher(_query);
+		
+        while(match.find()){
+            _tokens.add(match.group(1));
+            //phraseCount++;
+        }
+        
+        _query = _query.replace("\"(.*?)\"", " ");
+        
+        Scanner s = new Scanner(_query);
+        
+        while(s.hasNext()){
+            _tokens.add(s.next());
+        }
+        s.close();
 	}
+    
+	/*
+     public void processQuery(){
+     HashSet<String> set = new HashSet<String>();
+     
+     if (_query == null)
+     return;
+     System.out.println("Query is:" + _query);
+     _query = _query.replaceAll("%20", " ");
+     _query = _query.replaceAll("%22", "\"");
+     String[] tokens = _query.split("[ \"]");
+     for(String token: tokens){
+     if(!token.equals("")){
+     _tokens.add(token);
+     }
+     }
+     tokens = _query.split("[\"]");
+     for(int i = 1; i< tokens.length; i=i+2){
+     String[] phrase = tokens[i].split("[ ]");
+     phrases.add(phrase);
+     }
+     
+     for(int i = 0; i < phrases.size(); i++){
+     System.out.println("phrase:");
+     for(int j = 0; j < phrases.get(i).length; j++){
+     System.out.println(phrases.get(i)[j] + " ");
+     set.add(phrases.get(i)[j]);
+     }
+     }
+     
+     for(int i = 0; i < _tokens.size(); i++){
+     System.out.println(_tokens.get(i) + " ");
+     if(!set.contains(_tokens.get(i))){
+     nonPhrase.add(_tokens.get(i));
+     }
+     }
+     System.out.println();
+     }*/
+    
+    /* public static void main(String args[]){
+     String query = "[\"new%20york%20times\"][\"micro%20soft\"]%20film";
+     QueryPhrase test = new QueryPhrase(query);
+     test.processQuery();
+     
+     for(int i = 0; i < test._tokens.size(); i++){
+     System.out.println(test._tokens.get(i));
+     }
+     //System.out.println(test.getPhraseCount());
+     /*	for(String t: test._tokens){
+     System.out.println(t);
+     }
+     
+     */
+    /*
+     for(int i = 0; i < test.startIndex.size(); i++){
+     System.out.println(test._tokens.get(i));
+     }
+     for(int i = 0; i < test.endIndex.size(); i++){
+     System.out.println(test._tokens.get(i));
+     }
+     
+     System.out.println(test.endIndex.size());*/
+
 
 }
